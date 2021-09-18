8e8dec4a698d3c9f6b156c2b784fd8478af5c0e9
Guilherme Machado <machadofguilherme@gmail.com>
2021-09-18 03:01:55 -0300

    Pedra, Papel e Tesoura | UI

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,98 +4,96 @@ let pontosJogador = 0;
 let jogadaPC;
 let maoUsuario;
 
-for(let i = 1; i <= 5; i++) {
-    // Convite inicial ao usuário.
-    let jogadaUsuario = prompt('Escolha uma opção entre: pedra, papel e tesoura. \
-                            Adicione essa opção no campo abaixo.');
+// Referências.
+let mensagemInfo   = document.querySelector('.round-message');
+let mensagemRodada = document.querySelector('.msg-important');
+let btnPedra       = document.querySelector('#rock-btn');
+let btnTesoura     = document.querySelector('#scissor-btn');
+let btnPapel       = document.querySelector('#paper-btn');
+let placarUsuario  = document.querySelector('#score-user');
+let placarPC       = document.querySelector('#score-pc');
+let year           = document.querySelector('#year');
 
-    // Teste de entrada do usuário.                            
-    if  (jogadaUsuario == null) {
-        alert('Insira um valor válido na caixa de texto!');
-        break;
-    } else if (jogadaUsuario === 'pedra' || jogadaUsuario === 'papel' || jogadaUsuario === 'tesoura' ||
-               jogadaUsuario === 'Pedra' || jogadaUsuario === 'Papel' || jogadaUsuario === 'Tesoura' ||
-               jogadaUsuario === 'PEDRA' || jogadaUsuario === 'PAPEL' || jogadaUsuario === 'TESOURA') {
-               jogadaUsuario = jogadaUsuario;
-               jogadorPC();
-    } else {
-        alert('[ERRO] Tente novamente.');
-        break;
-    }
+// 'Mão' do usuário.
+btnPedra.addEventListener('click', function() {
+    maoUsuario = 'Pedra';
+    jogadorPC()
+});
 
-    // 'Mão' do computador.
-    function jogadorPC() {
-        // Variáveis locais.
-        let escolhePedra      = 'pedra';
-        let escolhePapel      = 'papel';
-        let escolheTesoura    = 'tesoura';
-        let escolhaItems      = [escolhePedra, escolhePapel, escolheTesoura];
+btnPapel.addEventListener('click', function() {
+    maoUsuario = 'Papel';
+    jogadorPC()
+});
+
+btnTesoura.addEventListener('click', function() {
+    maoUsuario = 'Tesoura';
+    jogadorPC()
+});
+
+// 'Mão' do computador.
+function jogadorPC() {
+    let escolhePedra      = 'pedra'        
+    let escolhePapel      = 'papel';
+    let escolheTesoura    = 'tesoura';
+    let escolhaItems      = [escolhePedra, escolhePapel, escolheTesoura];
     
-        // Randomizando o valor de cada índice do array.
-        if (escolhaItems[Math.floor(Math.random() * escolhaItems.length)] == escolhePedra) {
-            jogadaPC = 'pedra';
-            return jogoExecutado();
-        } else if (escolhaItems[Math.floor(Math.random() * escolhaItems.length)] == escolhePapel) {
-            jogadaPC = 'papel';
-            return jogoExecutado();
-        } else {
-            jogadaPC = 'tesoura';
-            return jogoExecutado();
-        }
-    }
-
-    // Função do jogo.
-    function jogoExecutado() {
-        alert('Você e o computador já escolheram o modo de ataque. Prepare-se!');
-        if (jogadaPC == jogadaUsuario) {
-            alert(`Você escolheu ${jogadaUsuario} e o computador escolheu ${jogadaPC}. Um empate!`)
-            return jogadaUsuario;
-        } else if (jogadaPC == 'pedra' && jogadaUsuario == 'papel') {
-            alert(`Você venceu! ${jogadaUsuario} engole ${jogadaPC}!`)
-            pontosJogador += 1;
-        } else if (jogadaPC == 'papel' && jogadaUsuario == 'pedra') {
-            alert(`Você perdeu! ${jogadaPC} engole ${jogadaUsuario}!`)
-            pontosPC += 1;
-        } else if (jogadaPC == 'pedra' && jogadaUsuario == 'tesoura') {
-            alert(`Você perdeu! ${jogadaPC} quebra ${jogadaUsuario}!`)
-            pontosPC += 1;
-        } else if (jogadaPC == 'tesoura' && jogadaUsuario == 'pedra') {
-            alert(`Você venceu! ${jogadaUsuario} quebra ${jogadaPC}!`)
-            pontosJogador += 1;
-        } else if (jogadaPC == 'papel' && jogadaUsuario == 'pedra') {
-            alert(`Você perdeu! ${jogadaPC} engole ${jogadaUsuario}!`)
-            pontosPC += 1;
-        } else if (jogadaPC == 'pedra' && jogadaUsuario == 'papel') {
-            alert(`Você venceu! ${jogadaUsuario} engole ${jogadaPC}!`)
-            pontosJogador += 1;
-        } else if (jogadaPC == 'papel' && jogadaUsuario == 'tesoura') {
-            alert(`Você venceu! ${jogadaUsuario} corta ${jogadaPC}!`)
-            pontosJogador += 1;
-        } else if (jogadaPC == 'tesoura' && jogadaUsuario == 'papel') {
-            alert(`Você perdeu! ${jogadaPC} corta ${jogadaUsuario}!`)
-            pontosPC += 1;
-        } else if (jogadaPC == 'tesoura' && jogadaUsuario == 'pedra') {
-            alert(`Você venceu! ${jogadaUsuario} quebra ${jogadaPC}!`)
-            pontosJogador += 1;
-        } else if (jogadaPC == 'pedra' && jogadaUsuario == 'tesoura') {
-            alert(`Você perdeu! ${jogadaPC} quebra ${jogadaUsuario}!`)
-            pontosPC += 1;
-        } else if (jogadaPC == 'tesoura' && jogadaUsuario == 'papel') {
-            alert(`Você perdeu! ${jogadaPC} corta ${jogadaUsuario}!`)
-            pontosPC += 1;
-        } else if (jogadaPC == 'papel' && jogadaUsuario == 'tesoura') {
-            alert(`Você venceu! ${jogadaUsuario} corta ${jogadaPC}!`)
-            pontosJogador += 1;
-        }
+    // Randomizando o valor de cada índice do array.
+    if (escolhaItems[Math.floor(Math.random() * escolhaItems.length)] == escolhePedra) {
+        jogadaPC = 'Pedra';
+        return jogoExecutado();
+    } else if (escolhaItems[Math.floor(Math.random() * escolhaItems.length)] == escolhePapel) {
+        jogadaPC = 'Papel';
+        return jogoExecutado();
+    } else {
+        jogadaPC = 'Tesoura';
+        return jogoExecutado();
     }
 }
 
-if (pontosJogador >= 1 && pontosPC >= 1) {
-    alert('Houve empate.')
-} else if (pontosJogador == pontosPC) {
-    throw new Error();
-} else if (pontosJogador < pontosPC) {
-    alert('O computador venceu!')
-} else if (pontosJogador > pontosPC) {
-    alert('Você é o grande campeão!')
+// Execução do jogo.
+function jogoExecutado() {
+    mensagemInfo.innerHTML = 'A partida irá começar!';
+    if (jogadaPC == maoUsuario) {
+        mensagemInfo.innerHTML = 'Um empate!';
+    } else if (jogadaPC == 'Pedra' && maoUsuario == 'Papel') {
+        mensagemInfo.innerHTML = `${maoUsuario} engole ${jogadaPC}!`
+        placarUsuario.innerHTML = pontosJogador += 1;
+    } else if (jogadaPC == 'Papel' && maoUsuario == 'Pedra') {
+        mensagemInfo.innerHTML = `${jogadaPC} engole ${maoUsuario}!`
+        placarPC.innerHTML = pontosPC += 1;
+    } else if (jogadaPC == 'Pedra' && maoUsuario == 'Tesoura') {
+        mensagemInfo.innerHTML = `${jogadaPC} quebra ${maoUsuario}!`
+        placarPC.innerHTML = pontosPC += 1;
+    } else if (jogadaPC == 'Tesoura' && maoUsuario == 'Pedra') {
+        mensagemInfo.innerHTML = `${maoUsuario} quebra ${jogadaPC}!`
+        placarUsuario.innerHTML = pontosJogador += 1;
+    } else if (jogadaPC == 'Papel' && maoUsuario == 'Pedra') {
+        mensagemInfo.innerHTML = `${jogadaPC} engole ${maoUsuario}!`
+        placarPC.innerHTML = pontosPC += 1;
+    } else if (jogadaPC == 'Pedra' && maoUsuario == 'Papel') {
+        mensagemInfo.innerHTML = `${maoUsuario} engole ${jogadaPC}!`
+        placarUsuario.innerHTML = pontosJogador += 1;
+    } else if (jogadaPC == 'Papel' && maoUsuario == 'Tesoura') {
+        mensagemInfo.innerHTML = `${maoUsuario} corta ${jogadaPC}!`
+        placarUsuario.innerHTML = pontosJogador += 1;
+    } else if (jogadaPC == 'Tesoura' && maoUsuario == 'Papel') {
+        mensagemInfo.innerHTML = `${jogadaPC} corta ${maoUsuario}!`
+        placarPC.innerHTML = pontosPC += 1;
+    } else if (jogadaPC == 'Tesoura' && maoUsuario == 'Pedra') {
+        mensagemInfo.innerHTML = `${maoUsuario} quebra ${jogadaPC}!`
+        placarUsuario.innerHTML = pontosJogador += 1;
+    } else if (jogadaPC == 'Pedra' && maoUsuario == 'Tesoura') {
+        mensagemInfo.innerHTML = `${jogadaPC} quebra ${maoUsuario}!`
+        placarPC.innerHTML = pontosPC += 1;
+    } else if (jogadaPC == 'Tesoura' && maoUsuario == 'Papel') {
+        mensagemInfo.innerHTML = `${jogadaPC} corta ${maoUsuario}!`
+        placarPC.innerHTML = pontosPC += 1;
+    } else if (jogadaPC == 'Papel' && maoUsuario == 'Tesoura') {
+        mensagemInfo.innerHTML = `${maoUsuario} corta ${jogadaPC}!`
+        placarUsuario.innerHTML = pontosJogador += 1;
+    } 
 }
+
+// Mudança de ano.
+let ano = new Date();
+year.innerHTML = ano.getFullYear();
